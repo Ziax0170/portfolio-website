cbeeff977fc4f2e34c4f5af6e08a63cfa23aa8a2
zia <>
2021-01-04 16:32:02 +0100

    4th draft

---
+++ port.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", showStuff);
+
+function showStuff() {
+  let hidden = document.getElementById('hidden');
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
