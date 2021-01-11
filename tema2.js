b10b66e7660a7c4ac0d4d0ce71ecefe6f2402ad3
Ziax0170 <70881657+Ziax0170@users.noreply.github.com>
2021-01-11 22:04:49 +0100

    Add files via upload

---
+++ tema2.js
@@ -0,0 +1,73 @@
+function showStuff() {
+  let hidden = document.getElementById('hidden');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+function showStuff2() {
+  let hidden = document.getElementById('hidden2');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+function showStuff3() {
+  let hidden = document.getElementById('hidden3');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+function showStuff4() {
+  let hidden = document.getElementById('hidden4');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+function showStuff5() {
+  let hidden = document.getElementById('hidden5');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+function showStuff6() {
+  let hidden = document.getElementById('hidden6');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+function showStuff7() {
+  let hidden = document.getElementById('hidden7');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
+
+function showStuff8() {
+  let hidden = document.getElementById('hidden8');
+    event.preventDefault();
+  if (hidden.style.display == "none") {
+    hidden.style.display = "block"
+  } else {
+    hidden.style.display = "none"
+  }
+}
